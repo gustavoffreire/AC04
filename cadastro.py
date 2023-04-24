ef491094a0445ab88c726da0483e03e70ece6879
Gustavo Emynem Izidre Ribeiro <2200826@SALAS.AULAS>
2023-04-24 20:51:18 -0300

    Começo AC04

---
+++ cadastro.py
@@ -0,0 +1,32 @@
+
+# MYSQL_HOST = 'root'
+# MYSQL_USER = 'cleriston'
+# MYSQL_PASSWORD = 'mudar123'
+# MYSQL_DB = 'pessoas'
+
+from flask import Flask, request, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        idade = request.form['idade']
+        genero = request.form['genero']
+
+        conn = mysql.connector.connect(user='cleriston', password='mudar123', host='root', database='mysql5')
+        cursor = conn.cursor()
+        query = "INSERT INTO pessoas (nome, idade, genero) VALUES (%s, %s, %s)"
+        values = (nome, idade, genero)
+        cursor.execute(query, values)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    return render_template('gravar.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5050)
+
